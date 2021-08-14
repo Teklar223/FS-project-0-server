9b60d47a7eccee2c6be45d96c88e9bcd98a8955c
Teklar223 <yonaratner@gmail.com>
2021-08-14 11:06:14 +0300

    Deployable in Heroku

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express"); //init express
 const app = express();              //create an instance of express
 const cors = require("cors");
+require("dotenv").config();
 
 app.use(express.json()); //enables using json for DB purposes
 app.use(cors());
@@ -21,7 +22,9 @@ const likesRouter = require("./routes/Likes");
 app.use("/likes", likesRouter);
 
 db.sequelize.sync().then(() => {
-    app.listen(3001 , () => {
+    app.listen(process.env.PORT || 3001 , () => {
         console.log("Server running on port 3001");
     });
+}).catch((err) => {
+    console.log(err);
 });
